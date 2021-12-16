48633e329a6e844e98ed1dc6ae2ef539f0785243
saharoori <saharlaor@gmail.com>
2021-12-16 18:45:21 +0200

    Dice component with images

---
+++ src/components/Dice/Dice.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Dice1 from "../../assets/img/dice-1.png";
+import Dice2 from "../../assets/img/dice-2.png";
+import Dice3 from "../../assets/img/dice-3.png";
+import Dice4 from "../../assets/img/dice-4.png";
+import Dice5 from "../../assets/img/dice-5.png";
+import Dice6 from "../../assets/img/dice-6.png";
+import "./Dice.css";
+
+const diceImage = {
+  0: "",
+  1: Dice1,
+  2: Dice2,
+  3: Dice3,
+  4: Dice4,
+  5: Dice5,
+  6: Dice6,
+};
+
+const Dice = (props) => {
+  const diceEls = props.values.map((val, i) => (
+    <img key={`dice-${i}`} src={diceImage[val]} alt={`dice-${val}`}></img>
+  ));
+  return <div className="Dice">{diceEls}</div>;
+};
+
+export default Dice;
