cf3ded06ec44540f8ba8cd61512a019d7e9bd361
saharoori <saharlaor@gmail.com>
2021-12-16 15:17:53 +0200

    Game component with initial state

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -1,21 +1,38 @@
 import React from "react";
+import Controls from "../Controls/Controls";
+import Player from "../Player/Player";
+import "./Game.css";
 
 class Game extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
-      someKey: "someValue",
+      totalScore: { player1: 0, player2: 0 },
+      currentScore: 0,
+      currentPlayer: "player1",
+      winningScore: 100,
+      isWon: false,
     };
   }
 
   render() {
-    return <p>{this.state.someKey}</p>;
-  }
-
-  componentDidMount() {
-    this.setState({
-      someKey: "otherValue",
-    });
+    return (
+      <div id="Game">
+        <Player
+          totalScore={this.state.totalScore.player1}
+          currentScore={
+            this.state.currentPlayer === "player1" ? this.currentScore : 0
+          }
+        />
+        <Player
+          totalScore={this.state.totalScore.player2}
+          currentScore={
+            this.state.currentPlayer === "player2" ? this.currentScore : 0
+          }
+        />
+        <Controls />
+      </div>
+    );
   }
 }
 
