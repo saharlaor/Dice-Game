3339f7fc20aa0fe6593003a94997fdf99ac90e26
saharoori <saharlaor@gmail.com>
2021-12-16 17:15:50 +0200

    Controls component

--- src/components/Controls/Controls.jsx
+++ src/components/Controls/Controls.jsx
@@ -1,22 +1,16 @@
 import React from "react";
+import Dice from "../Dice/Dice";
+import "./Controls.css";
 
-class Controls extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      someKey: "someValue",
-    };
-  }
-
-  render() {
-    return <p>{this.state.someKey}</p>;
-  }
-
-  componentDidMount() {
-    this.setState({
-      someKey: "otherValue",
-    });
-  }
-}
+const Controls = (props) => {
+  return (
+    <div className="Controls">
+      <button>New Game</button>
+      <Dice values={props.randomValues} />
+      <button>New Game</button>
+      <button>New Game</button>
+    </div>
+  );
+};
 
 export default Controls;
