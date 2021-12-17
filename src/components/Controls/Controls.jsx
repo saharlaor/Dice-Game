57678b6342c53dbbcbc988ed3acc46bdfa89c33e
saharoori <saharlaor@gmail.com>
2021-12-18 00:12:44 +0200

    Responsiveness for small devices in portrait mode

--- src/components/Controls/Controls.jsx
+++ src/components/Controls/Controls.jsx
@@ -7,8 +7,10 @@ const Controls = (props) => {
     <div className="Controls">
       <button onClick={props.handleNewGameClick}>New Game</button>
       <Dice values={props.randomValues} />
-      <button onClick={props.handleRollDiceClick}>Roll Dice</button>
-      <button onClick={props.handleHoldClick}>Hold</button>
+      <div className="Controls__play-buttons">
+        <button onClick={props.handleRollDiceClick}>Roll Dice</button>
+        <button onClick={props.handleHoldClick}>Hold</button>
+      </div>
     </div>
   );
 };
