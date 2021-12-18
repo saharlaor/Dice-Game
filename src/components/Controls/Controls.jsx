52695ff526bd0a2ba6202d34fa952bbe11a5d4da
saharoori <saharlaor@gmail.com>
2021-12-18 14:38:56 +0200

    Play dice roll sounds on click instead of change

--- src/components/Controls/Controls.jsx
+++ src/components/Controls/Controls.jsx
@@ -1,6 +1,20 @@
 import React from "react";
 import Dice from "../Dice/Dice";
+import { Howl, Howler } from "howler";
+import DiceRoll1 from "../../assets/audio/dice-roll1.mp3";
+import DiceRoll2 from "../../assets/audio/dice-roll2.mp3";
+import DiceRoll3 from "../../assets/audio/dice-roll3.mp3";
 import "./Controls.css";
+
+const SOUNDS = [DiceRoll1, DiceRoll2, DiceRoll3].map(
+  (sound) => new Howl({ src: sound })
+);
+
+const handleRollDiceClick = (handler) => {
+  Howler.volume(0.5);
+  SOUNDS[Math.floor(Math.random() * SOUNDS.length)].play();
+  handler();
+};
 
 const Controls = (props) => {
   return (
@@ -8,7 +22,9 @@ const Controls = (props) => {
       <button onClick={props.handleNewGameClick}>New Game</button>
       <Dice values={props.randomValues} />
       <div className="Controls__play-buttons">
-        <button onClick={props.handleRollDiceClick}>Roll Dice</button>
+        <button onClick={() => handleRollDiceClick(props.handleRollDiceClick)}>
+          Roll Dice
+        </button>
         <button onClick={props.handleHoldClick}>Hold</button>
       </div>
     </div>
--- src/components/Dice/Dice.jsx
+++ src/components/Dice/Dice.jsx
@@ -1,8 +1,4 @@
 import React from "react";
-import { Howl, Howler } from "howler";
-import DiceRoll1 from "../../assets/audio/dice-roll1.mp3";
-import DiceRoll2 from "../../assets/audio/dice-roll2.mp3";
-import DiceRoll3 from "../../assets/audio/dice-roll3.mp3";
 import Dice0 from "../../assets/img/dice-0.png";
 import Dice1 from "../../assets/img/dice-1.png";
 import Dice2 from "../../assets/img/dice-2.png";
@@ -12,42 +8,25 @@ import Dice5 from "../../assets/img/dice-5.png";
 import Dice6 from "../../assets/img/dice-6.png";
 import "./Dice.css";
 
-class Dice extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      diceImage: {
-        0: Dice0,
-        1: Dice1,
-        2: Dice2,
-        3: Dice3,
-        4: Dice4,
-        5: Dice5,
-        6: Dice6,
-      },
-      sounds: [DiceRoll1, DiceRoll2, DiceRoll3].map(
-        (sound) => new Howl({ src: sound })
-      ),
-    };
-  }
+const DICE_IMAGES = {
+  0: Dice0,
+  1: Dice1,
+  2: Dice2,
+  3: Dice3,
+  4: Dice4,
+  5: Dice5,
+  6: Dice6,
+};
 
-  render() {
-    const diceEls = this.props.values.map((val, i) => (
-      <img
-        key={`dice-${i}`}
-        src={this.state.diceImage[val]}
-        alt={`dice-${val}`}
-        style={val ? { boxShadow: "0 0 10px 5px #ccc" } : {}}></img>
-    ));
-    return <div className="Dice">{diceEls}</div>;
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    Howler.volume(0.5);
-    this.state.sounds[
-      Math.floor(Math.random() * this.state.sounds.length)
-    ].play();
-  }
-}
+const Dice = (props) => {
+  const diceEls = props.values.map((val, i) => (
+    <img
+      key={`dice-${i}`}
+      src={DICE_IMAGES[val]}
+      alt={`dice-${val}`}
+      style={val ? { boxShadow: "0 0 10px 5px #ccc" } : {}}></img>
+  ));
+  return <div className="Dice">{diceEls}</div>;
+};
 
 export default Dice;
